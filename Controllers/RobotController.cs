8167a5841a7011eeca8d67f354fc638e38e7f11a
Lior Matsliah <liormatsliah1985@gmail.com>
2023-05-07 18:34:50 +0300

    Add project files.

---
+++ Controllers/RobotController.cs
@@ -0,0 +1,40 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Logging;
+using Robotic_Arm_Control.Handlers;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Robotic_Arm_Control.Controllers
+{
+    [ApiController]
+    [Route("[controller]")]
+    public class RobotController : ControllerBase
+    {
+        private readonly ILogger<RobotController> _logger;
+        private readonly int _gridSize;
+
+        public RobotController(int gridSize = 100)
+        {
+            _gridSize = gridSize;
+        }
+
+        [HttpGet]
+        public int[] TrackRobot(string[] instructions)
+        {
+            int[] currentPosition = new int[] { 0, 0 };
+
+            try
+            {
+                currentPosition = RobotTracker.Track(instructions, _gridSize);
+            }
+            catch (Exception e)
+            {
+                _logger.LogError(e, "Error in TrackRobot. " + e.Message);
+            }
+
+            return currentPosition;
+        }
+    }
+}
---
+++ Handlers/RobotTracker.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Robotic_Arm_Control.Handlers
+{
+    public static class RobotTracker
+    {
+        public static int[] Track(string[] instructions, int gridSize)
+        {
+            int[] currentPosition = new int[] { 0, 0 };
+
+            foreach (var inst in instructions)
+            {
+                var parts = inst.Split(' ');
+                var direction = parts[0];
+                int distance = int.Parse(parts[1]);
+
+                switch (direction)
+                {
+                    case "right":
+                        currentPosition[0] = Math.Min(currentPosition[0] + distance, gridSize);
+                        break;
+                    case "left":
+                        currentPosition[0] = Math.Max(currentPosition[0] - distance, -gridSize);
+                        break;
+                    case "up":
+                        currentPosition[1] = Math.Min(currentPosition[1] + distance, gridSize);
+                        break;
+                    case "down":
+                        currentPosition[1] = Math.Max(currentPosition[1] - distance, -gridSize);
+                        break;
+                    default:
+                        throw new ArgumentException("Invalid instruction: " + inst);
+                }
+            }
+
+            return currentPosition;
+        }
+
+        public static void Output(int[] finalPosition)
+        {
+            Console.WriteLine("Final Position: ({0}, {1})", finalPosition[0], finalPosition[1]);
+        }
+    }
+}
