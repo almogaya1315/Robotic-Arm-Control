68fd09964434f2ee5a09b1727b8613c592b43d3f
Lior Matsliah <liormatsliah1985@gmail.com>
2023-05-07 18:41:13 +0300

    TesterConsole

---
+++ Handlers/TesterConsole.cs
@@ -0,0 +1,32 @@
+﻿using Robotic_Arm_Control.Controllers;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Robotic_Arm_Control.Handlers
+{
+    public class TesterConsole
+    {
+        static void Main(string[] args)
+        {
+            var tracker = new RobotController();
+
+            string[] instructions = new string[] { "right 10", "up 50", "left 30", "down 10" };
+            int[] finalPosition = tracker.TrackRobot(instructions);
+            RobotTracker.Output(finalPosition);
+
+            instructions = new string[0];
+            finalPosition = tracker.TrackRobot(instructions);
+            RobotTracker.Output(finalPosition);
+
+            instructions = new string[] { "right 100", "right 100", "up 500", "up 10000" };
+            finalPosition = tracker.TrackRobot(instructions);
+            RobotTracker.Output(finalPosition);
+
+            tracker = new RobotController(100000);
+            finalPosition = tracker.TrackRobot(instructions);
+            RobotTracker.Output(finalPosition);
+        }
+    }
+}
